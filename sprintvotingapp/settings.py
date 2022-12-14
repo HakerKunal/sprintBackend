294b50b6663b605cf9acc982c161edf2819bfea5
Kunal Batham <kunalba@emids.com>
2022-12-14 17:08:37 +0530

    wadwa

--- sprintvotingapp/settings.py
+++ sprintvotingapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2!+d=y74+1^$v=w5ntq!iq14fa!$$s61sm=q&o1rm3$tei5%2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
